b1a8d83b7d13f987281fb6983a03f67e130dd736
Dino René Caballero Marquez <dino.caballero@gmail.com>
2025-07-22 18:57:55 -0300

    corrección

--- layout/Customize mini.sh
+++ layout/Customize mini.sh
@@ -8,8 +8,8 @@ echo "---------------------------------------"
 # Obtener el directorio del usuario actual
 USER_HOME=$(eval echo ~$USER)
 
-# Ruta principal del tema Mini (en función del directorio del usuario)
-THEME_DIR="$USER_HOME/.emulationstation/themes/mini"
+# Ruta principal del tema Pi-Station-X (en función del directorio del usuario)
+THEME_DIR="$USER_HOME/.emulationstation/themes/Mini-main"
 
 # Ruta donde se almacenan las plantillas
 LAYOUT_DIR="$THEME_DIR/layout"
@@ -32,7 +32,7 @@ for key in "${!TEMPLATES[@]}"; do
     MENU_OPTIONS+=("$key" "${TEMPLATES[$key]}")
 done
 
-CHOICE=$(dialog --title "Personalizar 'Mini' / Mini'" \
+CHOICE=$(dialog --title "Personalizar 'Pi Station X' / Customize 'Pi Station X'" \
                 --menu "Selecciona una plantilla para aplicar: / Select a template to apply:" \
                 15 50 8 \
                 "${MENU_OPTIONS[@]}" 3>&1 1>&2 2>&3)
